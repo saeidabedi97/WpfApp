07bf9f95c42ed50df1c513139483e1b18cbbd61f
saeidabedi97 <saeidabedi51@gmail.com>
2020-04-29 23:06:17 +0200

    made the design

---
+++ Server/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Net;
+namespace Server
+
+{
+    /// <summary>
+    /// Interaction logic for server.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpServer server1;
+
+        private void Server_Load(object sender, EventArgs e)
+        {
+            server1 = new SimpleTcpServer();
+            server1.Delimiter = 0x13;
+            server1.DataReceived += Server_DataReceived;
+
+
+        }
+        private void Server_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            txtstatus.Dispatcher.Invoke((Action)delegate ()
+            {
+                txtstatus.Text += e.MessageString;
+                e.ReplyLine(string.Format("you said: {0}", e.MessageString));
+            });
+
+
+
+
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            txtstatus.Text = "Server starting...\n";
+            IPAddress ip = IPAddress.Parse(txthost.Text);
+            server1.Start(ip, Convert.ToInt32(txtport.Text));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (server1.IsStarted)
+                server1.Stop();
+        }
+
+        
+    }
+    }
+    
+
+
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        SimpleTcpClient client;
+
+        private void btn_start_Click(object sender, RoutedEventArgs e)
+        {
+            txtstatus.Text += "server starting...";
+            btn_connect.IsEnabled = false;
+            client.Connect(txthost.Text, Convert.ToInt32(txtport.Text));
+        }
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            client = new SimpleTcpClient();
+            client.StringEncoder = Encoding.UTF8;
+            client.DataReceived += Client_DataReceived;
+
+
+        }
+
+        private void Client_DataReceived(object sender, Message e)
+        {
+            txtstatus.Dispatcher.Invoke((Action)delegate ()
+            {
+                txtstatus.Text += e.MessageString;
+               
+            });
+        }
+
+        private void btn_send_Click(object sender, RoutedEventArgs e)
+        {
+            client.WriteLineAndGetReply(txtmessage.Text, TimeSpan.FromSeconds(3));
+        }
+    }
+}
